9b15b892c879108c13bcd7b66f4e5876e799aae6
Zoe Van Der Vlugt <122763958+zoevandervlugt@users.noreply.github.com>
2025-02-22 18:06:40 +0000

    Inputs & reads txt file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 // Main function of the C++ program.
 
-#include <iostream>
+#include <bits/stdc++.h>
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+    // Open example text file
+    std::fstream file;
+    file.open("examplefromdranthony.txt");  // In codeboard this needs to be Root/examplefromdranthony.txt
+        
+    if(file.is_open()) {
+        // String variable to store and read data
+        std::string dataString;
+    
+        // Read file and print until completely read
+        while(getline(file, dataString))
+        std::cout << dataString << std::endl;
+        
+        // Close the file
+        file.close();
+        return 1;
+    } else {
+        std::cout << strerror(errno) << std::endl;
+        return -1;
+    }
 }
